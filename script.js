02b2a17d0b94d503855eb91cf0f39c6a26037ec3
raunak-mishra <mishraraunak326@gmail.com>
2022-06-14 16:32:01 +0530

    modify html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var buttons = document.getElementsByClassName("button");
 var display = document.getElementById("display");
 
+// display.textContent = 0;
 var operand1 = 0;
 var operand2 = null;
 var operator = null;
@@ -9,34 +10,31 @@ function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
-for(var i=0;i<buttons.length;i++){
-    buttons[i].addEventListener('click', function(){
-        var value =  this.getAttribute('data-value');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+
+        var value = this.getAttribute('data-value');
         var text = display.textContent.trim();
-        if(isOperator(value)){
+
+        if (isOperator(value)) {
             operator = value;
             operand1 = parseFloat(text);
             display.textContent = "";
-        }
-        else if(value == "ac"){
+        } else if (value == "ac") {
             display.textContent = "";
-        }
-        else if(value == "sign"){
+        } else if (value == "sign") {
             operand1 = parseFloat(text);
             operand1 = -1 * operand1;
             display.textContent = operand1;
-        }
-        else if(value == "."){
-            if(text.length && !text.includes('.')){
+        } else if (value == ".") {
+            if (text.length && !text.includes('.')) {
                 display.textContent = text + '.';
             }
-        }
-        else if(value == "%"){
+        } else if (value == "%") {
             operand1 = parseFloat(text);
             operand1 = operand1 / 100;
             display.textContent = operand1
-        }
-        else if (value == "=") {
+        } else if (value == "=") {
             operand2 = parseFloat(text);
             var result = eval(operand1 + ' ' + operator + ' ' + operand2);
             if (result) {
@@ -45,8 +43,7 @@ for(var i=0;i<buttons.length;i++){
                 operand2 = null;
                 operator = null;
             }
-        }
-        else{
+        } else {
             display.textContent += value;
         }
     });
